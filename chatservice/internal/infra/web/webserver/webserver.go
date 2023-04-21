b70007bab5b9a3918e52bd64d6ab24a0f82bb3c8
medina325 <medina_cdz@hotmail.com>
2023-04-21 09:17:29 -0400

    Adicionando script main, com webservers e suas configs

---
+++ chatservice/internal/infra/web/webserver/webserver.go
@@ -0,0 +1,36 @@
+package webserver
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+type WebServer struct {
+	Router        chi.Router
+	Handlers      map[string]http.HandlerFunc // basicamente um dict pelo jeito, onde em string é passada a chave, e o valor retornado é um http.HandlerFunc
+	WebServerPort string
+}
+
+func NewWebServer(webServerPort string) *WebServer {
+	return &WebServer{
+		WebServerPort: webServerPort,
+		Router:        chi.NewRouter(),
+		Handlers:      make(map[string]http.HandlerFunc),
+	}
+}
+
+func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
+	s.Handlers[path] = handler
+}
+
+func (s *WebServer) Start() {
+	s.Router.Use(middleware.Logger)
+	for path, handler := range s.Handlers {
+		s.Router.Handle(path, handler)
+	}
+	if err := http.ListenAndServe(s.WebServerPort, s.Router); err != nil {
+		panic(err.Error())
+	}
+}
