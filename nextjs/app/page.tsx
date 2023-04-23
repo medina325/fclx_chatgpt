0b3ece7731363b4cb77128210f937952de22f82c
medina325 <medina_cdz@hotmail.com>
2023-04-23 00:07:14 -0400

    Finalizando estilização com tailwindCSS, marked, hightlight.js

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -4,15 +4,84 @@
 import ClientHttp, { fetcher } from "@/http/http";
 import { Chat, Message } from "@prisma/client";
 import { useRouter, useSearchParams } from "next/navigation";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useLayoutEffect, useState } from "react";
 import useSWR from "swr";
 import useSWRSubscription from "swr/subscription";
+import { PlusIcon } from "./components/PlusIcon";
+import { MessageIcon } from "./components/MessageIcon";
+import { ArrowRightIcon } from "./components/ArrowRightIcon";
+import Image from "next/image";
+import { UserIcon } from "./components/UserIcon";
+import { marked } from "marked";
+import hljs from "highlight.js";
+
+marked.setOptions({
+    highlight: function (code: string, lang: string) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+});
 
 // Criando um tipo que consiste em ser um Chat e um JSON que tem uma
 // chave messages, cujo valor é uma lista de objetos Message do prisma
 type ChatWithFirstMessage = Chat & {
     messages: [Message]
 }
+  
+function ChatItemError({ children }: { children: any }) {
+    return (
+        <li className="w-full text-gray-100 bg-gray-800">
+        <div className="md:max-w-2xl lg:max-w-xl xl:max-w-3xl py-6 m-auto flex flex-row items-start space-x-4">
+            <Image src="/fullcycle_logo.png" width={30} height={30} alt="" />
+            <div className="relative w-[calc(100%-115px)] flex flex-col gap-1">
+            <span className="text-red-500">Ops! Ocorreu um erro: {children}</span>
+            </div>
+        </div>
+        </li>
+    );
+}
+
+const Loading = () => (
+    <span className="animate-spin bg-white h-6 w-[5px] rounded"></span>
+);
+
+function ChatItem({
+    content,
+    is_from_bot,
+    loading = false,
+  }: {
+    content: string;
+    is_from_bot: boolean;
+    loading?: boolean;
+  }) {
+    const background = is_from_bot ? "bg-gray-800" : "bg-gray-600";
+  
+    return (
+      <li className={`w-full text-gray-100 ${background}`}>
+        <div className="flex-col">
+          <div className="md:max-w-2xl lg:max-w-xl xl:max-w-3xl py-6 m-auto flex flex-row items-start space-x-4">
+            {is_from_bot ? (
+              <Image src="/fullcycle_logo.png" width={30} height={30} alt="" />
+            ) : (
+              <UserIcon className="w-[30px] flex flex-col relative start" />
+            )}
+  
+            <div
+              className="relative w-[calc(100%-115px)] flex flex-col gap-1 transition duration-100 ease-linear break-words"
+              dangerouslySetInnerHTML={{
+                __html: marked(content, { breaks: true }), //sanitize: true
+              }}
+            />
+          </div>
+          {loading && (
+            <div className="flex items-center justify-center pb-2">
+              <Loading />
+            </div>
+          )}
+        </div>
+      </li>
+    );
+  }
 
 export default function Home() {
     const router = useRouter();
@@ -104,8 +173,24 @@ export default function Home() {
                 form.requestSubmit(submitBtn); // Muito estranho, não seria melhor apenas clicar no botão?
                 return;
             }
+
+            if (textArea.scrollHeight >= 200) {
+                textArea.style.overflowY = "scroll";
+            } else {
+                textArea.style.overflowY = "hidden";
+                textArea.style.height = "auto";
+                textArea.style.height = textArea.scrollHeight + "px";
+            }
         });
     }, []);
+
+    useLayoutEffect(() => {
+        if (!messageLoading) {
+          return;
+        }
+        const chatting = document.querySelector("#chatting") as HTMLUListElement;
+        chatting.scrollTop = chatting.scrollHeight;
+    }, [messageLoading]);
 
     async function onSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -131,30 +216,79 @@ export default function Home() {
     }
 
     return (
-        <div className="flex gap-5">
-            <div className="flex flex-col">
-                <button type="button" onClick={() => router.push("/")}>New chat</button>
-                <ul>
+        <div className="overflow-hidden w-full h-full flex">
+            <div className="bg-gray-900 w-[300px] h-screen p-2 flex flex-col">
+                <button 
+                    type="button" 
+                    onClick={() => {router.push("/"); setChatId(null); setMessageId(null);}} 
+                    className="flex p-3 gap-3 rounded hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-1 border border-white/20"
+                >
+                    <PlusIcon className="w-5 h-5"/>
+                    New chat
+                </button>
+                <ul className="overflox-y-auto">
                     {chats!.map((chat, key) => (
-                        <li key={key} onClick={() => router.push(`/?id=${chat.id}`)}>
-                            {chat.messages[0]?.content}
+                        <li
+                            key={key}
+                            className="pb-2 text-gray-100 text-sm mr-2"
+                        >
+                            <button
+                                onClick={() => router.push(`/?id=${chat.id}`)}
+                                className="flex p-3 gap-3 w-full hover:bg-[#3f4679] rounded group"
+                            >
+                                <MessageIcon className="w-5 h-5"/>
+                                <div className="overflow-hidden w-full text-left max-h-5 relative">
+                                    {chat.messages[0]?.content}
+                                    <div className="absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#3f4679]"></div>
+                                </div>
+                            </button>
                         </li>
                     ))}
                 </ul>
             </div>
-            <div>
-                <ul>
-                    Centro
-                    {messages!.map((message, key) => (
-                        <li key={key}>{message.content}</li>
+            <div className="flex-1 flex-col relative">
+                <ul className="h-screen overflow-y-auto bg-gray-800">
+                    {messages?.map((message, key) => (
+                        <ChatItem
+                            key={key}
+                            content={message.content}
+                            is_from_bot={message.is_from_bot}
+                        />
                     ))}
-                    {messageLoading && <li>{messageLoading}</li>}
-                    {errorMessageLoading && <li>{errorMessageLoading}</li>}
+                    {messageLoading && (
+                        <ChatItem
+                            content={messageLoading}
+                            is_from_bot={true}
+                            loading={true}
+                        />
+                    )}
+                    {errorMessageLoading && (
+                        <ChatItemError>{errorMessageLoading}</ChatItemError>
+                    )}
+                    <li className="h-36 bg-gray-800"></li>
                 </ul>
-                <form id="form" onSubmit={ onSubmit }>
-                    <textarea id="message" placeholder="Digite sua pergunta" className="text-black"></textarea>
-                    <button type="submit">Enviar</button>
-                </form>
+                <div className="absolute w-full bottom-0 bg-gradient-to-b from-gray-800 to-gray-950">
+                    <div className="mb-6 mx-auto max-w-3xl">
+                        <form id="form" onSubmit={ onSubmit }>
+                            <div className="flex flex-col relative py-3 pl-4 text-white bg-gray-700 rounded">
+                                <textarea
+                                    id="message"
+                                    tabIndex={0}
+                                    rows={1}
+                                    placeholder="Digite sua pergunta"
+                                    className="resize-none pr-14 bg-transparent pl-0 outline-none"
+                                ></textarea>
+                                <button
+                                    type="submit"
+                                    className="absolute top-1 text-gray-400 bottom-2.5 rounded hover:text-gray-400 hover:bg-gray-900 md:right-4"
+                                    disabled={messageLoading}
+                                >
+                                    <ArrowRightIcon className="text-white-500 w-8"/>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     )
